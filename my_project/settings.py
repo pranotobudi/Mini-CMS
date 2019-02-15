32295e8d9f4edd32bf04d6307bb0eb591c0be74b
pranotobudi <pranotobudi.s@gmail.com>
2019-02-15 16:53:48 +0700

    move database from sqlite3 to postgresql

--- my_project/settings.py
+++ my_project/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'minicms',
+        'USER': 'minicms',
+        'PASSWORD': 'passwordminicms',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+#ADMIN: username: admin, password:admin
 
 
 # Password validation
